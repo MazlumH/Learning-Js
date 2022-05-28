cbdd9f40e56e9b9ee0f0e3645d834878dc752a4c
Mazlum Havutcu <106154097+MazlumH@users.noreply.github.com>
2022-05-28 22:30:26 +0200

    Add files via upload

---
+++ Functions.js
@@ -0,0 +1,9 @@
+// Functions sind funktionen die eine logische handlung durchführen.
+/*Aufbau: function log() {
+   console.log(log) 
+}*/
+let zahl = 1
+
+function log() {
+    console.log(zahl)
+}
